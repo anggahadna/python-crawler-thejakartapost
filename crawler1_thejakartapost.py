dc99643666e478c03c7cc2e4bf8401777ce8e1b4
Angga Hadna <AnggaHadna@gmail.com>
2019-02-14 19:07:59 +0700

    add cat4, new case 'id = show-define-text'

--- crawler1_thejakartapost.py
+++ crawler1_thejakartapost.py
@@ -20,11 +20,12 @@ all_text.append(title)
 all_text.append(date_time)
 all_text.append(border)
 
-# detect the right category from the article
+# detect the right category of the article
 
 cat1 = soup.find_all('div', {'class' : 'col-md-10 col-xs-12 detailNews'}) #news
 cat2 = soup.find_all('div', {'class' : 'col-md-10 col-xs-12 detailNews wdsk wtbl'}) #multimedia
 cat3 = soup.find_all('div', {'class' : 'show-define-text'}) #lifestyle, travel
+cat4 = soup.find_all('div', {'id' : 'show-define-text'})
 
 if len(cat1) == 1:
     cat = cat1
@@ -32,11 +33,13 @@ elif len(cat2) == 1:
     cat = cat2
 elif len(cat3) == 1:
     cat = cat3
-
+elif len(cat4) == 1:
+    cat = cat4
+    
 # take all text with "p" tag under specific tag and class
-
-for news in cat:
-    for n in news.descendants:
+    
+for article in cat:
+    for n in article.descendants:
         if n.name == 'p':
             paragraphs = '\n' + n.text
             all_text.append(paragraphs)
